a3a5cd41b77384f7fc5f002727b1704e1d9e15b0
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-05 14:04:06 +0545

    Add Users list and UserDetails component for dynamic routing

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Link from "next/link";
 
 const Users = () => {
-  return <div>Users Data Dashboard</div>;
+  return (
+    <div>
+      <h1>Users Data Dashboard</h1>
+
+      <ul className="mt-10 ml-10">
+        <li><Link rel="stylesheet" href="/dashboard/users/1">User1</Link></li>
+        <li><Link rel="stylesheet" href="/dashboard/users/2">User2</Link></li>
+        <li><Link rel="stylesheet" href="/dashboard/users/3">User3</Link></li>
+        <li><Link rel="stylesheet" href="/dashboard/users/4">User4</Link></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Users;
